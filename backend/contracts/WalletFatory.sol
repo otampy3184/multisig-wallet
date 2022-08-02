4eae455ab26434d8d0dc9d4c7b91bbed2822fc49
otampy3184 <o.tampodo.t.18@gmail.com>
2022-08-02 15:46:16 +0900

    WalletFactoryの生成関数作成中

--- backend/contracts/WalletFatory.sol
+++ backend/contracts/WalletFatory.sol
@@ -1,8 +1,22 @@
 // SPDX-License-Identifier: unlicensed
 pragma solidity ^0.8.0;
 
+import './MultisigWallet.sol';
+
 // Ownerごとに新しくMultisigWalletを作成するため、ownerからの呼び出しはFactoryContractで受ける
 contract WalletFactory {
     // MultiSigWallet型の配列
-    MultiSigWallet[] public wallets;    
+    MultisigWallet[] public wallets;    
+
+    // Walletインスタンス生成用のイベント
+    event WalletCreated (MultisigWallet indexed wallet, string name, address[] owners, uint required);
+
+    // MultisigWalletのインスタンス生成メソッド
+    function createWallet (
+        string memory _name,
+        address[] memory _owners,
+        uint _required
+    ) public {
+        //MultisigWallet wallet = new MultisigWallet(_name, _owners, _required);
+    }
 }
