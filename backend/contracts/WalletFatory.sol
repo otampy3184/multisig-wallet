3c677d14839a1e83e49ce203887fa67188acc451
otampy3184 <o.tampodo.t.18@gmail.com>
2022-08-12 16:10:10 +0900

    getWallets function

--- backend/contracts/WalletFatory.sol
+++ backend/contracts/WalletFatory.sol
@@ -6,7 +6,9 @@ import './MultisigWallet.sol';
 // Ownerごとに新しくMultisigWalletを作成するため、ownerからの呼び出しはFactoryContractで受ける
 contract WalletFactory {
     // MultiSigWallet型の配列
-    MultisigWallet[] public wallets;    
+    MultisigWallet[] public wallets;  
+
+    uint256 constant maxLimit = 20;  
 
     // Walletインスタンス生成用のイベント
     event WalletCreated (MultisigWallet indexed wallet, string name, address[] owners, uint required);
@@ -26,9 +28,17 @@ contract WalletFactory {
         emit WalletCreated(wallet, _name, _owners, _required);
     }
 
-    function getWallets(uint limit, uint256 offset) public view returns (MultisigWallet) {
-        //sakuseityu
+    function getWallets(uint limit, uint256 offset) public view returns (MultisigWallet[] memory coll) {
+        require(offset <= walletsCount(), "offset out of bounds");
+        uint256 size = walletsCount() - offset;
+        size = size < limit ? size : limit;
+        size = size < maxLimit ? size : maxLimit;
+        coll = new MultisigWallet[](size);
+
+        for ( uint256 i = 0; i < size; i++){
+            coll[i] = wallets[offset + i];
+        }
+
+        return coll;
     }
-
-
 }
